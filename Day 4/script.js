bfd3fdbb4d77bb8831a67f9eefb930ff8e01f0bc
RahmaaEsmail <108898486+RahmaaEsmail@users.noreply.github.com>
2023-02-10 19:26:50 +0200

    upload day-4

---
+++ Day 4/script.js
@@ -0,0 +1,29 @@
+// Leetcode move-zeroes
+// move all zero to the right of array
+
+// let nums = [0, 1, 0, 3, 12];
+// var moveZeroes = function (nums) {
+
+//     for (const i of nums) {
+//         let index = nums.indexOf(0);
+//         if (index > -1) {
+//             nums.splice(index, 1);
+//             nums.push(0)
+//         }
+//     }
+//     return nums;
+// };
+
+// console.log(moveZeroes(nums));
+
+
+
+// Hackerrank 30 day of code
+//  Day 2 operator
+// function solve(meal_cost, tip_percent, tax_percent) {
+//     let tip = (tip_percent/100) * meal_cost;
+//     let tax = (tax_percent/100) * meal_cost;
+//     console.log(Math.round(tip + tax + meal_cost));
+// }
+
+
