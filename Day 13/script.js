7201db0b706526733d20b463424df3c7dbf331b3
RahmaaEsmail <108898486+RahmaaEsmail@users.noreply.github.com>
2023-02-27 03:12:48 +0200

    upload day-13

---
+++ Day 13/script.js
@@ -0,0 +1,38 @@
+/////////////////////////////////////CODEWARS/////////////////////////////
+// Write Number in Expanded Form
+// function expandedForm(num) {
+//     return num.toString().split("").reverse().map((a,i)=>a*(10**i)).filter(a=>a>0).reverse().join(" + ")
+// }
+// console.log(expandedForm(12));
+
+
+
+// Who likes it ?
+// function likes(names) {
+//     if (names.length === 0) return "no one likes this";
+//     if (names.length === 1) return `${names[0]} likes this`;
+//     if (names.length === 2) return `${names[0]} and ${names[1]} like this`;
+//     if (names.length === 3) return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+//     if (names.length > 3) return `${names[0]}, ${names[1]} and ${names.length-2} others like this`;
+// }
+// console.log(likes(["Alex", "Jacob", "Mark", "Max"]));
+
+
+
+// Sentence Smash
+// function smash(words) {
+//     return words.join(" ").trim();
+// };
+// console.log(smash(['hello', 'world', 'this', 'is', 'great']));
+
+
+
+// sum of SeriesSum
+// function SeriesSum(n) {
+//     let x = 0 ;
+//     for(let i=0;i<n;i++)
+//       x+= (1/(i*3+1));
+//     return x.toFixed(2);
+// }
+// console.log(SeriesSum(5));
+
