5fd411c9254c856ae5339f361642d5e93ae29fa1
RahmaaEsmail <108898486+RahmaaEsmail@users.noreply.github.com>
2023-03-22 07:22:17 +0200

    upload day-20

---
+++ Day 20/script.js
@@ -0,0 +1,66 @@
+// The Greatest
+
+// let a = parseInt(prompt("enter 1 : "));
+// let b = parseInt(prompt("enter 1 : "));
+// let c = parseInt(prompt("enter 1 : "));
+// let maiorAB = (a + b + Math.abs(a - b)) / 2;
+// let greatestValue = Math.max(maiorAB,c);
+// console.log(`${greatestValue} eh o maior`);
+
+
+
+// Area
+
+// let A = parseFloat(prompt("enter 1 : "));
+// let B = parseFloat(prompt("enter 1 : "));
+// let C = parseFloat(prompt("enter 1 : "));
+
+// const PI = 3.14159
+// let TRIANGULO = (0.5*A*C).toFixed(3);
+// let CIRCULO = (PI*(C**2)).toFixed(3);
+// let TRAPEZIO = (0.5*(A+B)*C).toFixed(3);
+// let QUADRADO = (B*B).toFixed(3);
+// let RETANGULO = (A*B).toFixed(3);
+
+// console.log(`TRIANGULO: ${TRIANGULO}`);
+// console.log(`CIRCULO: ${CIRCULO}`); 
+// console.log(`TRAPEZIO: ${TRAPEZIO}`); 
+// console.log(`QUADRADO: ${QUADRADO}`); 
+// console.log(`RETANGULO: ${RETANGULO}`); 
+
+
+// The Feast of Many Beasts
+// function feast(beast, dish) {
+//     let fB = beast.split(" ")[0][0];
+//     let lB = beast.split(" ").at(-1).at(-1);
+//     let fD = dish.split(" ")[0][0];
+//     let lD = dish.split(" ").at(-1).at(-1);
+//     return fB === fD && lB === lD;
+// }
+// console.log(feast("brown bear", "bear claw"));
+
+
+// Twice as old
+// function twiceAsOld(dadYearsOld, sonYearsOld) {
+//     return Math.abs((sonYearsOld * 2) - dadYearsOld) ;
+// }
+// console.log(twiceAsOld(36,7));
+
+
+// Count the divisors of a number
+// function getDivisorsCnt(n) {
+//     let c =0;
+//     for(let i = 1;i<=n;i++) {
+//          if(n%i==0)
+//           c++;
+//     }
+//     return c;
+// }
+// console.log(getDivisorsCnt(4));
+
+
+// Parse nice int from char problem
+// function getAge(inputString) {
+//      return Number(inputString[0]);
+// }
+// console.log(getAge("4 years old"));
